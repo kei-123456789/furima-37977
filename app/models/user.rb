c5aa6d8968ae82be65c6c353b1faccd19399b4a9
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-05-14 21:46:00 +0900

    Itemモデルの作成、マイグレーション、アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,5 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶ一]+\z/}
   validates :birthday, presence: true
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-
+  has_many :items
 end
