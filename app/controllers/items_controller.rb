ad481d75f043447585f88d606976fdb6191ffe80
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-05-15 21:01:34 +0900

    アクティブハッシュ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,21 @@ class ItemsController < ApplicationController
   def index
   end
   def new
+    @item = Item.new
   end
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :explain, :price, :category_id, :condition_id, :charge_id, :prefecture_id, :number_id, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  validates :title, presence: true
-  validates :explain, presence: true
+  validates :title, :explain, :image, presence: true
   validates :price, presence: true, numericality: {greater_than: 299,less_than: 10000000}
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :charge_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :number_id, presence: true
-  validates :image, presence: true
+  validates :category_id, :condition_id, :charge_id, :prefecture_id, :number_id ,numericality: { other_than: 1 }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :charge, :prefecture, :number
+
 end
