5a83195a7a4d4d4bff0cb3a609cc092f94a57c5c
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-05-16 16:48:00 +0900

    ビューファイルとアソシエーションの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,9 @@ class Item < ApplicationRecord
   validates :category_id, :condition_id, :charge_id, :prefecture_id, :number_id ,numericality: { other_than: 1 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :charge, :prefecture, :number
-
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :number
 end
