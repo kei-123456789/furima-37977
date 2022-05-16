0bfd6e77f63fbed1a54234112c4df8b0dc6bf87d
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-05-16 14:43:34 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,9 @@ class ItemsController < ApplicationController
   def index
   end
   def new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
     @item = Item.new
   end
   def create
