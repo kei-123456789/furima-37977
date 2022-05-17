3dce01f80e27cfbbefa91fb88c123d2790c5cc2a
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-05-17 17:25:33 +0900

    バリデーション

--- app/models/delively.rb
+++ app/models/delively.rb
@@ -1,3 +1,7 @@
 class Delively < ApplicationRecord
   belongs_to :order
+  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+  validates :prefecture_id,numericality: { other_than: 1 }
+  validates :municipalities, :address, presence: true
+  validates :telephone_number, presence: true, format: {with: /\A0[0-9]{9,10}\z/}
 end
