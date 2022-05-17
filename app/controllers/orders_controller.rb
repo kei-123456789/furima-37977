d51079a9400d790a9bd2e4f3f75658c4fc3cec83
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-05-17 20:17:30 +0900

    決済機能関連

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,5 +7,29 @@ class OrdersController < ApplicationController
   def new
   end
   def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit().merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
+
 end
