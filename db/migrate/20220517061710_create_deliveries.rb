6340fc8279980493fe7da2cf82c3cbf44b0c4377
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-05-19 18:15:42 +0900

    m

--- db/migrate/20220517061710_create_deliveries.rb
+++ db/migrate/20220517061710_create_deliveries.rb
@@ -1,6 +1,6 @@
-class CreateDelivelies < ActiveRecord::Migration[6.0]
+class CreateDeliveries < ActiveRecord::Migration[6.0]
   def change
-    create_table :delivelies do |t|
+    create_table :deliveries do |t|
       t.string :postal_code,      null: false
       t.integer :prefecture_id,   null: false
       t.string :municipalities,   null: false
--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2022_05_17_061710) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "delivelies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "deliveries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
     t.integer "prefecture_id", null: false
     t.string "municipalities", null: false
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2022_05_17_061710) do
     t.bigint "order_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["order_id"], name: "index_delivelies_on_order_id"
+    t.index ["order_id"], name: "index_deliveries_on_order_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -89,7 +89,7 @@ ActiveRecord::Schema.define(version: 2022_05_17_061710) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "delivelies", "orders"
+  add_foreign_key "deliveries", "orders"
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
