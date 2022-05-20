ab1ebb38eb74cc2aa11d1ff437096cdd80c973e1
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-05-20 22:09:38 +0900

    複数枚画像、ほとんどコードコピー。。。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :order
-  has_one_attached :image
-  validates :title, :explain, :image, presence: true
+  has_many_attached :images
+  validates :title, :explain, presence: true
   validates :price, presence: true, numericality: {only_integer: true, greater_than: 299,less_than: 10000000}
   validates :category_id, :condition_id, :charge_id, :prefecture_id, :number_id ,numericality: { other_than: 1 }
+  validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
